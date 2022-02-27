fe070b349e2410f246c9033e12384189550d53a0
tongthanhthu <tongthanhthu27062k@gmail.com>
2022-02-27 13:11:05 +0700

    PHP oop 2

---
+++ MainExercise.php
@@ -0,0 +1,74 @@
+<?php
+
+abstract class Supervisor
+{
+    protected $slogan ;
+
+    abstract public function saySloganOutLoud();
+
+    public function setSlogan ($slogan) {
+        $this->slogan = $slogan;
+        //var_dump($this->slogan);die();
+    }
+}
+
+interface Boss {
+  public function checkValidSlogan();
+}
+
+class EasyBoss extends Supervisor implements Boss
+{
+    public function saySloganOutLoud() {
+        $before = 'before';
+        $after = 'after';
+
+        return strpos($this->slogan, $before) !== false || strpos($this->slogan, $after) !== false;
+        
+    }
+    public function checkValidSlogan(){
+        if($this->saySloganOutLoud() !== false){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
+}
+
+
+class UglyBoss extends Supervisor implements Boss {
+    public function saySloganOutLoud() {
+        $before = 'before';
+        $after = 'after';
+
+       return strpos($this->slogan, $before) === false || strpos($this->slogan, $after) === false;
+    }
+    public function checkValidSlogan(){
+        if($this->saySloganOutLoud() !== false){
+            return true;
+        }
+        else{
+            return false;
+        }
+        
+    }
+}
+$easyBoss = new EasyBoss();
+$uglyBoss = new UglyBoss();
+
+$easyBoss->setSlogan('Do NOT push anything to master branch before reviewed by supervisor(s)');
+
+$uglyBoss->setSlogan('Do NOT push anything to master branch before reviewed by supervisor(s). Only they can do it after check it all!');
+
+$easyBoss->saySloganOutLoud(); 
+echo "<br>";
+$uglyBoss->saySloganOutLoud(); 
+
+echo "<br>";
+
+var_dump($easyBoss->checkValidSlogan()); // true
+echo "<br>";
+var_dump($uglyBoss->checkValidSlogan()); // true
+
+
