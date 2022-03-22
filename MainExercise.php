92393b289eb8761e01b4a393b3fb8ab54e8a3566
tongthanhthu <96277544+tongthanhthu@users.noreply.github.com>
2022-03-22 16:19:45 +0700

    Update MainExercise.php

--- MainExercise.php
+++ MainExercise.php
@@ -42,7 +42,7 @@ class UglyBoss extends Supervisor implements Boss {
         $before = 'before';
         $after = 'after';
 
-       return strpos($this->slogan, $before) === false && strpos($this->slogan, $after) === false;
+       return strpos($this->slogan, $before) !== false && strpos($this->slogan, $after) !== false;
     }
     public function checkValidSlogan(){
         if($this->saySloganOutLoud() !== false){
