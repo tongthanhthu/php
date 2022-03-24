9d60f1631449e110d0c4ce9f9eab51a046527b36
tongthanhthu <96277544+tongthanhthu@users.noreply.github.com>
2022-03-24 19:12:46 +0700

    Update User.php

--- thu/app/Models/User.php
+++ thu/app/Models/User.php
@@ -39,7 +39,7 @@ class User extends Model
      return true;
     }
     public function listAll(){
-      return User::orderBy('id','asc')->Search()->paginate(20);
+      return User::orderBy('mail_address','asc')->Search()->paginate(20);
     }
     public function scopeSearch($query){
        if( $key = request()->key){
