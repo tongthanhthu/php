40322e59383038edaeb4aa6d7083335fd455b366
tongthanhthu <96277544+tongthanhthu@users.noreply.github.com>
2022-03-29 14:13:43 +0700

    Update UsersController.php

--- thu/app/Http/Controllers/UsersController.php
+++ thu/app/Http/Controllers/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Http\Requests\CreateUserRequest;
+use Session;
 
 class UsersController extends Controller
 {
